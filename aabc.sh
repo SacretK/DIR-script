146f5c735f461165d3b67e10f70400fcf1d1f474
SacretK <83625072+SacretK@users.noreply.github.com>
2021-06-03 00:51:01 -1200

    Update aabc.sh

--- aabc.sh
+++ aabc.sh
@@ -2,7 +2,7 @@
 aa=4 
 ab=5 
 ac=6
-let “aa = aa + ac”
-echo “The first operation is equal to $aa” 
-let “ac = ac - ab”
-echo “The second operation is equal to $ac”
+let "aa = aa+ac"
+echo "The first operation is equal to $aa"
+let "ac = ac - ab"
+echo "The second operation is equal to $ac"
